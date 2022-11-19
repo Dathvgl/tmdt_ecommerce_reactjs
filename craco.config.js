0f7f86bbb6c3fcb61462ad6b74f52e1769a03ad5
Dathvgl <99634251+Dathvgl@users.noreply.github.com>
2022-11-19 08:59:24 +0700

    fix node

--- craco.config.js
+++ craco.config.js
@@ -1,41 +1,41 @@
-// const fs = require("fs");
-// const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
-// const evalSourceMapMiddleware = require("react-dev-utils/evalSourceMapMiddleware");
-// const noopServiceWorkerMiddleware = require("react-dev-utils/noopServiceWorkerMiddleware");
-// const redirectServedPath = require("react-dev-utils/redirectServedPathMiddleware");
+const fs = require("fs");
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+const evalSourceMapMiddleware = require("react-dev-utils/evalSourceMapMiddleware");
+const noopServiceWorkerMiddleware = require("react-dev-utils/noopServiceWorkerMiddleware");
+const redirectServedPath = require("react-dev-utils/redirectServedPathMiddleware");
 
-// module.exports = {
-//   webpack: {
-//     plugins: {
-//       add: [
-//         new NodePolyfillPlugin({
-//           excludeAliases: ["console"],
-//         }),
-//       ] /* An array of plugins */,
-//     },
-//   },
-//   devServer: (devServerConfig, { env, paths }) => {
-//     devServerConfig = {
-//       onBeforeSetupMiddleware: undefined,
-//       onAfterSetupMiddleware: undefined,
-//       setupMiddlewares: (middlewares, devServer) => {
-//         if (!devServer) {
-//           throw new Error("webpack-dev-server is not defined");
-//         }
+module.exports = {
+  webpack: {
+    plugins: {
+      add: [
+        new NodePolyfillPlugin({
+          excludeAliases: ["console"],
+        }),
+      ] /* An array of plugins */,
+    },
+  },
+  devServer: (devServerConfig, { env, paths }) => {
+    devServerConfig = {
+      onBeforeSetupMiddleware: undefined,
+      onAfterSetupMiddleware: undefined,
+      setupMiddlewares: (middlewares, devServer) => {
+        if (!devServer) {
+          throw new Error("webpack-dev-server is not defined");
+        }
 
-//         if (fs.existsSync(paths.proxySetup)) {
-//           require(paths.proxySetup)(devServer.app);
-//         }
+        if (fs.existsSync(paths.proxySetup)) {
+          require(paths.proxySetup)(devServer.app);
+        }
 
-//         middlewares.push(
-//           evalSourceMapMiddleware(devServer),
-//           redirectServedPath(paths.publicUrlOrPath),
-//           noopServiceWorkerMiddleware(paths.publicUrlOrPath)
-//         );
+        middlewares.push(
+          evalSourceMapMiddleware(devServer),
+          redirectServedPath(paths.publicUrlOrPath),
+          noopServiceWorkerMiddleware(paths.publicUrlOrPath)
+        );
 
-//         return middlewares;
-//       },
-//     };
-//     return devServerConfig;
-//   },
-// };
+        return middlewares;
+      },
+    };
+    return devServerConfig;
+  },
+};
